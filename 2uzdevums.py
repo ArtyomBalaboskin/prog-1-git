4d2a862ecc95ae39fc9b11e881083f5d962b3b55
ArtyomBalaboskin <artem.balaboskin@gmail.com>
2021-02-10 08:53:49 +0200

    2 uzdevums(2)

---
+++ 2uzdevums.py
@@ -0,0 +1,16 @@
+import math
+def gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums):
+    telpas_izmers = math.ceil(telpas_garums) * math.ceil(telpas_platums)
+    izmaksa = telpas_izmers / linoleja_platums
+
+    return izmaksa
+
+cena1 = 2.50
+linoleja_platums1 = 2.0
+platums1 = 5.0
+garums1 = 6.0
+
+print("izklājot garumā:")
+print(gridas_izmaksa(cena1, linoleja_platums1, platums1, garums1))
+print("izklājot platumā")
+print(gridas_izmaksa(cena1, linoleja_platums1, garums1, platums1))
