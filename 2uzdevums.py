606434638698580f788886510f1d6d8a3e1668cf
ArtyomBalaboskin <77140952+ArtyomBalaboskin@users.noreply.github.com>
2021-02-12 08:58:15 +0200

    2(2)uzdevums.py

--- 2uzdevums.py
+++ 2uzdevums.py
@@ -1,3 +1,4 @@
+#1 Variants
 import math
 def gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums):
     telpas_izmers = math.ceil(telpas_garums) * math.ceil(telpas_platums)
@@ -9,6 +10,27 @@ cena1 = 2.50
 linoleja_platums1 = 2.0
 platums1 = 5.0
 garums1 = 6.0
+
+print("izklājot garumā:")
+print(gridas_izmaksa(cena1, linoleja_platums1, platums1, garums1))
+print("izklājot platumā")
+print(gridas_izmaksa(cena1, linoleja_platums1, garums1, platums1))
+
+#2 Variants
+import math
+
+#Data
+platums1 = float(input('Platums: '))
+garums1 = float(input('Garums: '))
+
+def gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums):
+    telpas_izmers = math.ceil(telpas_garums) * math.ceil(telpas_platums)
+    izmaksa = telpas_izmers / linoleja_platums
+
+    return izmaksa
+
+cena1 = 2.50
+linoleja_platums1 = 2.0
 
 print("izklājot garumā:")
 print(gridas_izmaksa(cena1, linoleja_platums1, platums1, garums1))
