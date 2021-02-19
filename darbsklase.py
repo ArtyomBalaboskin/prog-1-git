2a1f93b757c8e1685531b0572bc3950134176645
ArtyomBalaboskin <77140952+ArtyomBalaboskin@users.noreply.github.com>
2021-02-19 09:19:53 +0200

    darbsklase

---
+++ darbsklase.py
@@ -0,0 +1,10 @@
+a = float(input ("Первое число "))
+b = float(input ("Второе число "))
+c = float(input ("Третье число "))
+if (b<a>c):
+ print(a)
+if (a<b>c):
+ print(b)
+if (a<c>b):
+ print(c)
+ 
